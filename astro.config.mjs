57f812d47f03ddf63bddb15d51decfc75acdde47
Joev101stack <johann@stacksolver.tech>
2025-10-14 09:38:51 -0500

    feat: Instala y configura dependencias (SEO, Sitemap, Compress)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+// INICIO DE MODIFICACIÓN
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
+// FIN DE MODIFICACIÓN
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // INICIO DE MODIFICACIÓN
+  site: 'https://www.wasvelt.com', // Dominio final del sitio web
+  integrations: [sitemap(), compress()]
+  // FIN DE MODIFICACIÓN
+});
